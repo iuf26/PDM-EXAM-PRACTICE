81a210f4e82d01b1320739b8737a8400ea00ceea
iulia.AirportLabs <iulia.filimon@airportlabs.com>
2023-01-21 15:34:40 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'client-1',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
